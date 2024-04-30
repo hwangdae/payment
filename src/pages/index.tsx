4b2987d52761414bb9b585b23bed243474a63704
hwang daeseong <klp2797@naver.com>
2024-04-30 21:46:13 +0900

    feat: 프리텐다드 폰트 적용 및 퍼블리싱 80% 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,29 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Checkbox } from "@/components/ui/checkbox"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { registerSchema } from "@/valitators/delivery";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
+import shortid from "shortid";
 
-const items = [{id:"",label:"",},{id:"",label:"",},{id:"",label:"",},{id:"",label:"",},{id:"",label:"",},]
+const items = [
+  { id: "토스페이", label: "토스페이" },
+  { id: "핸드폰 결제", label: "핸드폰 결제" },
+  { id: "카카오페이", label: "카카오페이" },
+  { id: "신용카드", label: "신용카드" },
+  { id: "무통장 입금", label: "무통장 입금" },
+];
 
 const index = () => {
   const form = useForm<z.infer<typeof registerSchema>>({
@@ -33,215 +49,250 @@ const index = () => {
   return (
     <div>
       <div className="m-5 border border-solid px-5">
-        <header className="divide-y mb-5">
+        <header className="border-b-2 mb-5">
           <h1 className="text-4xl font-[700] py-5">Order/Payment</h1>
         </header>
-        <main className="flex justify-between gap-5">
-          <div className="w-[70%]  border">
-          <section className="mb-5">
-            <h2>주문 상품 정보</h2>
-          </section>
+        <main>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <section className="mb-5">
-                <h2>주문자 정보</h2>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>이름</FormLabel>
-                      <FormControl>
-                        <Input placeholder="이름을 입력해 주세요." {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>이메일</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="이메일을 입력해 주세요."
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>휴대폰</FormLabel>
-                      <FormControl>
-                        <Input placeholder="01012345678(- 제외)" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </section>
-              <section  className="mb-5">
-                <h2>배송 정보</h2>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>이름</FormLabel>
-                      <FormControl>
-                        <Input placeholder="수령인" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="ordername"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>배송지명(선택)</FormLabel>
-                      <FormControl>
-                        <Input placeholder="배송지명" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>휴대폰</FormLabel>
-                      <FormControl>
-                        <Input placeholder="01012345678(- 제외)" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="landLinePhone"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>전화번호</FormLabel>
-                      <FormControl>
-                        <Input placeholder="01012345678(- 제외)" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />                        
-                <FormField
-                  control={form.control}
-                  name="address"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>배송지 주소</FormLabel>
-                      <FormControl>
-                        <Input placeholder="주소" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="address"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input placeholder="상세 주소" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                /><FormField
-                control={form.control}
-                name="address"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>배송지 주소</FormLabel>
-                    <FormControl>
-                      <Input placeholder="주소" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              </section>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className={cn("flex justify-between gap-5")}
+            >
+              <div className="w-[70%]">
+                <section>
+                  <h2>주문 상품 정보</h2>
+                  <div className="border p-5 my-5"></div>
+                </section>
+                <section>
+                  <h2>주문자 정보</h2>
+                  <div className="border p-5 my-5">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>이름</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="이름을 입력해 주세요."
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>이메일</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="이메일을 입력해 주세요."
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="phone"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>휴대폰</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="01012345678(- 제외)"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </section>
+                <section>
+                  <h2>배송 정보</h2>
+                  <div className="border p-5 my-5">
+                    <FormField
+                      control={form.control}
+                      name="name"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>이름</FormLabel>
+                          <FormControl>
+                            <Input placeholder="수령인" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="ordername"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>배송지명(선택)</FormLabel>
+                          <FormControl>
+                            <Input placeholder="배송지명" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="phone"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>휴대폰</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="01012345678(- 제외)"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="landLinePhone"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>전화번호</FormLabel>
+                          <FormControl>
+                            <Input
+                              placeholder="01012345678(- 제외)"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="address"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>배송지 주소</FormLabel>
+                          <FormControl>
+                            <Input placeholder="주소" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="address"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel></FormLabel>
+                          <FormControl>
+                            <Input placeholder="상세 주소" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </section>
+                <section>
+                  <h2>쿠폰/포인트</h2>
+                  <div className="border p-5 my-5"></div>
+                </section>
+              </div>
+              <aside className="w-[30%]">
+                <section className="mb-5">
+                  <h2>최종 결제금액</h2>
+                  <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[100px]">Invoice</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </section>
+                <section>
+                  <h2>결제 방법</h2>
+                  <div className="mt-5 border p-5">
+                    <h3 className="mb-5">결제 수단</h3>
+                    <FormField
+                      control={form.control}
+                      name="items"
+                      render={() => (
+                        <FormItem className={cn("flex flex-col")}>
+                          {items.map((item) => (
+                            <FormField
+                              key={item.id}
+                              control={form.control}
+                              name="items"
+                              render={({ field }) => {
+                                return (
+                                  <FormItem
+                                    key={item.id}
+                                    className="flex flex-row items-start space-x-3 space-y-1 justify-start w-[100%]"
+                                  >
+                                    <FormControl>
+                                      <Checkbox
+                                        checked={field.value?.includes(item.id)}
+                                        onCheckedChange={(checked) => {
+                                          return checked
+                                            ? field.onChange([
+                                                // ...field.value,
+                                                item.id,
+                                              ])
+                                            : field.onChange(
+                                                field.value?.filter(
+                                                  (value) => value !== item.id
+                                                )
+                                              );
+                                        }}
+                                      />
+                                    </FormControl>
+                                    <FormLabel className="font-normal">
+                                      {item.label}
+                                    </FormLabel>
+                                  </FormItem>
+                                );
+                              }}
+                            />
+                          ))}
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="bg-slate-200 p-3">
+                    <p>
+                      <span className="text-blue-500 font-bold">160</span>{" "}
+                      포인트 적립 예정
+                    </p>
+                  </div>
+                </section>
+              </aside>
             </form>
           </Form>
-          <section>
-            <h2>쿠폰/포인트</h2>
-          </section>
-          </div>
-          <aside>
-          <section className="mb-5">
-            <h2>최종 결제금액</h2>
-          </section>
-          <section className="mb-5">
-            <h2>결제 방법</h2>
-            <FormField
-          control={form.control}
-          name="items"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormLabel className="text-base">Sidebar</FormLabel>
-                <FormDescription>
-                  Select the items you want to display in the sidebar.
-                </FormDescription>
-              </div>
-              {items.map((item) => (
-                <FormField
-                  key={item.id}
-                  control={form.control}
-                  name="items"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(item.id)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  )
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="font-normal">
-                          {item.label}
-                        </FormLabel>
-                      </FormItem>
-                    )
-                  }}
-                />
-              ))}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-          </section>
-          </aside>
         </main>
       </div>
     </div>
