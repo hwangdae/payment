89dd4ead4f839d545e38f28e646f74f98048f8a3
hwang daeseong <klp2797@naver.com>
2024-05-14 20:53:08 +0900

    feat: 상품 옵션 선택 페이지 구현 완료

--- src/pages/detail/[id]/index.tsx
+++ src/pages/detail/[id]/index.tsx
@@ -24,11 +24,20 @@ import {
 import { cn } from "@/lib/utils";
 import shortid from "shortid";
 
+interface ItemsType {
+  id: string;
+  size: string;
+  category?: string | undefined;
+  image?: string | undefined;
+  maker?: string | undefined;
+  description?: string | undefined;
+  quantity?: number | undefined;
+  price?: number | undefined;
+}
+
 const Detail = () => {
   const router = useRouter();
   const { id } = router.query;
-  const [quantity, setQuantity] = useState<any>(1);
-  const [selectedValue, setSelectedValue] = useState("");
   const [items, setItems] = useState<any>([]);
 
   const merchandise = MERCHANDISES.find((merchandise: MerchandiseType) => {
@@ -84,7 +93,11 @@ const Detail = () => {
       )
     );
   };
-
+  console.log(
+    items.map((item: any) => {
+      return item.price;
+    })
+  );
   const minusButtonHandler = (itemId: string) => {
     setItems((prevItems: any) =>
       prevItems.map((item: any) =>
@@ -100,14 +113,14 @@ const Detail = () => {
   };
 
   return (
-    <main className=" w-[100%]">
-      <h2 className="bg-gray-200 p-4 font-bold">{merchandise?.maker}</h2>
+    <main className="w-[80%]">
+      <h2 className="bg-gray-900 p-4 font-bold text-white">{merchandise?.maker}</h2>
       <h2 className="py-4 font-bold">{merchandise?.description}</h2>
       <div className="flex gap-4">
-        <h1 className="flex align-center justify-center border">
+        <h1 className="flex align-center justify-center border h-[600px]">
           <img src={merchandise?.image} alt="상품 이미지" />
         </h1>
-        <div>
+        <div className="w-[35%]">
           <div className="mb-6 pb-6 border-b-2">
             <h1 className="mb-3">
               Product Info
@@ -149,82 +162,115 @@ const Detail = () => {
               Price Info
               <span className="text-gray-300 ml-2 font-normal">가격정보</span>
             </h1>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                  control={form.control}
-                  name="size"
-                  render={({ field }) => (
-                    <FormItem
-                      className={cn(
-                        "flex justify-center border py-3 bg-gray-50 flex-col"
-                      )}
-                    >
-                      <Select
-                        onValueChange={(value) => {
-                          setSelectedValue(value);
-                          field.onChange(value);
-                          addItemList(value);
-                        }}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="옵션 선택" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="M">M</SelectItem>
-                          <SelectItem value="L">L</SelectItem>
-                          <SelectItem value="XL">XL</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                 {items.map((item: any) => {
-                  return (
-                    <div key={item.id} className="flex justify-between border">
-                      <span>{item.size}</span>
-                      <div className="flex gap-2">
-                        <button
-                          type="button"
-                          onClick={() => plusButtonHandler(item.id)}
-                        >
-                          +
-                        </button>
-                        <p>{item.quantity}</p>
-                        <button
-                          type="button"
-                          onClick={() => minusButtonHandler(item.id)}
-                        >
-                          -
-                        </button>
-                      </div>
-                      <div className="flex gap-2">
-                        <p>{item.price.toLocaleString()}원</p>
-                        <button onClick={()=>{
-                          
-                        }}>X</button>
-                      </div>
-                    </div>
-                  );
-                })}
-                  <div>
-                    <div className="flex items-center mb-3 justify-between border p-3">
-                      <span className="w-[80px]">총 상품 금액</span>
-                      <p className="font-bold">
-                        {merchandise?.price.toLocaleString()} 원
-                      </p>
-                    </div>
-                  </div>  
-                <Button type="submit" className={cn("w-[100%]")}>
-                  구매하기
-                </Button>
-              </form>
-            </Form>
+            <div className="flex items-center">
+              <span className="w-[80px]">상품 금액</span>
+              <p className="font-bold">
+                {merchandise?.price.toLocaleString()}원
+              </p>
+            </div>
           </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="size"
+                render={({ field }) => (
+                  <FormItem
+                    className={cn(
+                      "flex justify-center border py-3 bg-gray-50 flex-col"
+                    )}
+                  >
+                    <Select
+                      onValueChange={(value) => {
+                        field.onChange(value);
+                        addItemList(value);
+                      }}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="옵션 선택" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="M">M</SelectItem>
+                        <SelectItem value="L">L</SelectItem>
+                        <SelectItem value="XL">XL</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {items.map((item: MerchandiseType) => {
+                return (
+                  <div
+                    key={item.id}
+                    className="flex justify-between border px-4 py-2 text-sm items-center"
+                  >
+                    <span className="w-[40px]">{item.size}</span>
+                    <div className="flex w-[110px]">
+                      <button
+                        type="button"
+                        className={`border px-2 bg-gray-100 ${
+                          item.quantity <= 1 ? " text-gray-300" : ""
+                        }`}
+                        onClick={() => minusButtonHandler(item.id)}
+                      >
+                        -
+                      </button>
+                      <p className="border w-[38px] text-center">
+                        {item.quantity}
+                      </p>
+                      <button
+                        type="button"
+                        className="border px-2 bg-gray-100"
+                        onClick={() => plusButtonHandler(item.id)}
+                      >
+                        +
+                      </button>
+                    </div>
+                    <div className="flex text-gray-600">
+                      <p className="w-[82px]">
+                        {item.price.toLocaleString()}원
+                      </p>
+                      <button
+                        className="px-2"
+                        onClick={() => {
+                          setItems(
+                            items.filter((listItem: MerchandiseType) => {
+                              return listItem.id !== item.id;
+                            })
+                          );
+                        }}
+                      >
+                        x
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+              <div>
+                <div className="flex items-center mb-3 justify-between border p-3">
+                  <span className="w-[80px]">총 상품 금액</span>
+                  <p className="font-bold">
+                    {items
+                      .map((item: MerchandiseType) => {
+                        return item.price;
+                      })
+                      .reduce((acc: number, curr: number) => {
+                        return acc + curr;
+                      }, 0)
+                      .toLocaleString()}
+                    원
+                  </p>
+                </div>
+              </div>
+              <Button type="submit" className={cn("w-[100%] py-7")}>
+                구매하기
+              </Button>
+            </form>
+          </Form>
         </div>
       </div>
     </main>
